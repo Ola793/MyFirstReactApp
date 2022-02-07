a527c3b34f1ef3d84e4f485dc3e43bd667365d8c
Ola793 <67291449+Ola793@users.noreply.github.com>
2022-02-08 00:43:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Header
-        </p>
-      </header>
-
-      <main className="App-main">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Main block
-        </p>
-      </main>
-
-      <footer className="App-footer">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Footer
-        </p>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
